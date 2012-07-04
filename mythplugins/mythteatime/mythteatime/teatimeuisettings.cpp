11869052d77f5fcaae845151345bc116d951a11d
Tobias Maier <diespambox@gmx.net>
2012-07-04 20:30:31 +0200

    add setting to activate/deactivate pause playback on notification.

--- mythplugins/mythteatime/mythteatime/teatimeuisettings.cpp
+++ mythplugins/mythteatime/mythteatime/teatimeuisettings.cpp
@@ -8,7 +8,8 @@ TeaTimerSettings::TeaTimerSettings (MythScreenStack *parent)
     :MythScreenType(parent, "teatimesettings"),
     m_CancelButton(NULL),
     m_OkButton(NULL),
-    m_NotifyText(NULL)
+    m_NotifyText(NULL),
+    m_PausePlayback(NULL)
 {
 }
 bool TeaTimerSettings::Create()
@@ -29,6 +30,7 @@ bool TeaTimerSettings::Create()
     bool err = false;
     UIUtilE::Assign(this, m_OkButton, "ok", &err);
     UIUtilE::Assign(this, m_NotifyText, "notification_message", &err);
+    UIUtilE::Assign(this, m_PausePlayback, "pause_playback", &err);
     if (err)
     {
         LOG_Tea(LOG_WARNING, "Theme is missing required elements."); 
@@ -37,8 +39,11 @@ bool TeaTimerSettings::Create()
 
     QString notifyText =  gCoreContext->GetSetting("Teatime_NotificationText", 
                                                     tr("Tea is ready!"));
-
     m_NotifyText->SetText(notifyText);
+    bool pause =  gCoreContext->GetSetting("Teatime_PausePlayback", 
+                                                    "1") == "1";
+    m_PausePlayback->SetCheckState(pause);
+
     
     if (m_CancelButton)
         connect(m_CancelButton, SIGNAL(Clicked()), SLOT(Close()));
@@ -54,6 +59,7 @@ void TeaTimerSettings::save()
 {
 
     gCoreContext->SaveSetting("Teatime_NotificationText", m_NotifyText->GetText());
+    gCoreContext->SaveSetting("Teatime_PausePlayback", m_PausePlayback->GetBooleanCheckState()? "1":"0");
 
     LOG_Tea(LOG_INFO, "Saved teatime settings.");
     Close();
